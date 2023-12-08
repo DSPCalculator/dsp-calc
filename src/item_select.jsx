cee8d824099ca7b7bfdecda45eb44c60379c3f0f
JimmyZJX <x@201a.cn>
2023-12-09 00:33:56 +0800

    item select panel now supports multiple game mods

--- src/item_select.jsx
+++ src/item_select.jsx
@@ -1,14 +1,44 @@
+import structuredClone from '@ungap/structured-clone';
 import { Modal } from 'bootstrap';
-import { useEffect, useRef } from 'react';
+import { useContext, useEffect, useRef } from 'react';
 import { createPortal } from 'react-dom';
 import { icon_layout } from '../data/icon_layout.jsx';
+import { GameInfoContext } from './contexts.jsx';
 import { ItemIcon } from './recipe.jsx';
 
-// TODO this should depend on the game data
-
 function ItemSelectPanel({ onSelect }) {
-  const doms = icon_layout.map((row, i) => {
-    if (row.length == 0) return <div key={i} className="m-1"></div>;
+  const game_info = useContext(GameInfoContext);
+  let game_data = game_info.game_data;
+
+  let all_unused_targets = new Set(game_data.recipe_data
+    .flatMap(recipe => Object.keys(recipe["产物"])));
+
+  let icons = structuredClone(icon_layout);
+
+  for (let row of icons) {
+    for (let i in row) {
+      let item = row[i];
+      item = item.startsWith("*") ? item.substring(1) : item;
+      if (all_unused_targets.has(item)) {
+        all_unused_targets.delete(item);
+      } else {
+        row[i] = "";
+      }
+    }
+  }
+
+  let unused_targets = Array.from(all_unused_targets);
+  if (unused_targets.length > 0) {
+    icons.push([]);
+    const chunkSize = 12;
+    for (let i = 0; i < unused_targets.length; i += chunkSize) {
+      const chunk = unused_targets.slice(i, i + chunkSize);
+      icons.push(chunk);
+    }
+  }
+
+  const doms = icons.map((row, i) => {
+    if (row.length == 0) return <div key={i} className="m-2"></div>;
     let doms = row.map((r, i) => {
       // TODO is_ore
       let [is_ore, item] =
@@ -48,7 +78,9 @@ export function ItemSelect({ item, set_item, text, btn_class }) {
       onClick={() => ref_modal.current.show()}>
       {item && <><ItemIcon item={item} size={24} tooltip={false} />
         <span className="ms-1"></span></>}
-      <small className="text-nowrap">{item || text}</small>
+      {(item) ?
+        <small className="text-nowrap">{item}</small>
+        : text}
     </button>
 
     {createPortal(
