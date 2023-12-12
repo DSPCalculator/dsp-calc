de779f099a260baa81a2530efe3a5c24cc033202
JimmyZJX <x@201a.cn>
2023-12-13 01:11:58 +0800

    refactor item_select to CSS grid

--- src/item_select.jsx
+++ src/item_select.jsx
@@ -1,4 +1,3 @@
-import structuredClone from '@ungap/structured-clone';
 import { Modal } from 'bootstrap';
 import { useContext, useEffect, useRef, useState } from 'react';
 import { createPortal } from 'react-dom';
@@ -7,32 +6,26 @@ import { ItemIcon } from './recipe.jsx';
 import fuzzysort from 'fuzzysort'
 import { pinyin } from 'pinyin-pro';
 
-function ItemSelectPanel({ fuzz_result, onSelect, icons }) {
+function ItemSelectPanel({ fuzz_result, onSelect, icon_grid }) {
   let fuzz_set = new Set(fuzz_result);
 
-  const doms = icons.map((row, i) => {
-    if (row.length == 0) return <div key={i} className="m-2"></div>;
-    let doms = row.map((r, i) => {
-      // TODO is_ore
-      let [is_ore, item] =
-        r.startsWith("*") ? [true, r.substring(1)] : [false, r];
-
-      if (item == "")
-        return <div key={i} style={{ width: "48px", height: "48px" }}></div>;
-      else {
-        let class_opacity = fuzz_set.has(item) ? "" : "opacity-25";
-        return <div key={i}
-          className={`bg-body-secondary bg-opacity-10 cursor-pointer hover-bg-opacity-50 ${class_opacity}`}
-          onClick={() => onSelect(item)}>
-          <ItemIcon item={item} size={48} />
-        </div>;
-      }
-    });
-    return <div key={i} className="d-flex gap-1 px-2 w-fit">{doms}</div>;
+  const doms = icon_grid.icons.map(({ col, row, item }) => {
+    let class_opacity = fuzz_set.has(item) ? "" : "opacity-25";
+    return <div key={col + "#" + row}
+      className={`bg-body-secondary bg-opacity-10 cursor-pointer hover-bg-opacity-50 ${class_opacity}`}
+      style={{ gridRow: row, gridColumn: col }}
+      onClick={() => onSelect(item)}>
+      <ItemIcon item={item} size={48} />
+    </div>;
   })
 
-  return <div className="p-3 w-fit rounded-3">
-    <div className="d-flex flex-column gap-1 py-2 w-fit">{doms}</div>
+  return <div className="p-3 py-4 w-fit rounded-3 gap-1"
+    style={{
+      display: "grid",
+      gridTemplateColumns: `repeat(${icon_grid.ncol}, max-content)`,
+      gridTemplateRows: `repeat(${icon_grid.nrow}, max-content)`,
+    }}>
+    {doms}
   </div>;
 }
 
@@ -42,54 +35,10 @@ export function ItemSelect({ item, set_item, text, btn_class }) {
   const input_ref = useRef();
 
   const game_info = useContext(GameInfoContext);
-  let game_data = game_info.game_data;
-  let all_targets = game_data.recipe_data.flatMap(recipe => Object.keys(recipe["产物"]));
-  all_targets = Array.from(new Set(all_targets));
-  const [fuzz_result, set_fuzz_result] = useState(all_targets);
+  const all_target_items = game_info.all_target_items;
+  const [fuzz_result, set_fuzz_result] = useState([]);
 
-  let icon_layouts = new Array(32);
-  for (let i = 0; i < 32; i++) {
-    icon_layouts[i] = new Array(17).fill("");
-  }
-  let max_row = 8;
-  for (let item in game_info.item_data) {
-    if (!(item in game_data.item_grid)) {
-      //console.log(item);
-      continue;
-    }
-    let grid = game_data.item_grid[item];
-    if (grid > 4716) {
-      //console.log(item, grid);
-      continue;
-    }
-    icon_layouts[(Math.floor(grid / 1000) - 1) * max_row + (Math.floor((grid % 1000) / 100) - 1)][Math.floor((grid % 100) - 1)] = item;
-  }
-  let all_unused_targets = new Set(all_targets);
-  let icons = structuredClone(icon_layouts);
-
-  for (let row of icons) {
-    for (let i in row) {
-      let item = row[i];
-      item = item.startsWith("*") ? item.substring(1) : item;
-      if (all_unused_targets.has(item)) {
-        all_unused_targets.delete(item);
-      } else {
-        row[i] = "";
-      }
-    }
-  }
-
-  let unused_targets = Array.from(all_unused_targets);
-  if (unused_targets.length > 0) {
-    icons.push([]);
-    const chunkSize = 12;
-    for (let i = 0; i < unused_targets.length; i += chunkSize) {
-      const chunk = unused_targets.slice(i, i + chunkSize);
-      icons.push(chunk);
-    }
-  }
-
-  const search_targets = all_targets.map(item => ({
+  const search_targets = all_target_items.map(item => ({
     item: item,
     py_first: pinyin(item, { pattern: 'first', type: 'array' }).join(""),
     py_full: pinyin(item, { toneType: 'none' })
@@ -97,15 +46,18 @@ export function ItemSelect({ item, set_item, text, btn_class }) {
 
   const RESULT_LIMIT = 10;
 
+  useEffect(() => {
+    set_fuzz_result(game_info.all_target_items);
+  }, [game_info]);
+
   function do_search(value) {
     if (!value) {
-      set_fuzz_result(all_targets);
+      set_fuzz_result(all_target_items);
     } else {
       let search_result = fuzzysort.go(value, search_targets, {
         keys: ["item", "py_first", "py_full"],
         limit: RESULT_LIMIT,
       });
-      console.log(search_result);
       set_fuzz_result(search_result.map(e => e.obj.item));
     }
   }
@@ -161,7 +113,7 @@ export function ItemSelect({ item, set_item, text, btn_class }) {
       <div ref={ref} className="modal" tabIndex="-1">
         <div className="modal-dialog mw-fit">
           <div className="modal-content bg-dark flex-row" style={{ "--bs-bg-opacity": 0.85 }}>
-            <ItemSelectPanel fuzz_result={fuzz_result} icons={icons}
+            <ItemSelectPanel fuzz_result={fuzz_result} icon_grid={game_info.icon_grid}
               onSelect={on_select_item} />
             <div className="p-3 d-flex flex-column gap-2">
               <input ref={input_ref} className="round rounded-3 py-1 px-2 my-1" placeholder="搜索（支持拼音）"
