af062bb1ac8e44888e7a9f5dc1d594b260489dce
JimmyZJX <x@201a.cn>
2024-01-01 18:01:19 +0800

    fix css ordering

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Header } from './header.jsx';
 import App from './App.jsx';
+import { Header } from './header.jsx';
 
 import 'bootstrap/dist/js/bootstrap.min.js';
 import 'bootstrap/js/dist/dropdown.js';
-import '../css/App.css';
+
 import '../css/styles.scss';
+// app-specific CSS
+import '../css/App.css';
+
 
 ReactDOM.createRoot(document.getElementById('header')).render(
   <React.StrictMode>
