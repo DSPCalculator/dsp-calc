1425bdfa2b0e7b3586d7c7ec1361b8caa685a3dc
JimmyZJX <x@201a.cn>
2024-01-01 18:06:00 +0800

    整理navbar相关代码 (#5)
    * navbar cleanup
    
    * minor margin edit
    
    * fix css ordering
    
    * minor typo ("lassName")

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import { Header } from './header.jsx';
 
-import '../css/styles.scss'
-import '../css/App.css'
-import 'bootstrap/dist/js/bootstrap.min.js'
-import 'bootstrap/js/dist/dropdown.js'
+import 'bootstrap/dist/js/bootstrap.min.js';
+import 'bootstrap/js/dist/dropdown.js';
+
+import '../css/styles.scss';
+// app-specific CSS
+import '../css/App.css';
+
+
+ReactDOM.createRoot(document.getElementById('header')).render(
+  <React.StrictMode>
+    <Header />
+  </React.StrictMode>,
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
