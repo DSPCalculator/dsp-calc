3b5f29c678f0c30352befb881d464d098874c589
JimmyZJX <x@201a.cn>
2024-01-01 17:16:34 +0800

    navbar cleanup

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Header } from './header.jsx';
+import App from './App.jsx';
 
-import '../css/styles.scss'
-import '../css/App.css'
-import 'bootstrap/dist/js/bootstrap.min.js'
-import 'bootstrap/js/dist/dropdown.js'
+import 'bootstrap/dist/js/bootstrap.min.js';
+import 'bootstrap/js/dist/dropdown.js';
+import '../css/App.css';
+import '../css/styles.scss';
+
+ReactDOM.createRoot(document.getElementById('header')).render(
+  <React.StrictMode>
+    <Header />
+  </React.StrictMode>,
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
