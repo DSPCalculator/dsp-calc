116b9ff8756e40978abec62e29bb21aeb5740ed5
JimmyZJX <x@201a.cn>
2024-02-03 21:53:38 +0800

    fix flaky spritesmith generation

--- vite.config.js
+++ vite.config.js
@@ -1,35 +1,47 @@
 import react from '@vitejs/plugin-react';
 import { lstatSync, readdirSync } from 'fs';
+import fsp from 'fs/promises';
+import glob from 'glob';
 import imagemin from 'imagemin';
 import imageminPngquant from 'imagemin-pngquant';
 import imageminWebp from 'imagemin-webp';
 import path from 'path';
+import Spritesmith from 'spritesmith';
 import { defineConfig } from 'vite';
-import Spritesmith from 'vite-plugin-spritesmith';
 
 /** This generates one sprite image per game name when `mode == "development"` (`npm run dev`) */
 function get_sprite_plugins(mode) {
   if (mode == "development") return [];
 
-  return readdirSync('./icon').flatMap(dir => {
+  return readdirSync('./icon').map(dir => {
     if (lstatSync(`./icon/${dir}`).isDirectory()) {
       const output_icon = `./icon/${dir}.png`;
-      return [Spritesmith({
-        src: { cwd: `./icon/${dir}`, glob: '*.png' },
-        target: {
-          image: output_icon,
-          css: [[`./icon/${dir}.json`, { format: "json" }]],
-        },
-      }),
-      {
+
+      return {
         // convert `output_icon` to webp and compressed png
-        name: "vite:spritesmith:postprocess_image",
-        enforce: "post",
+        name: "spritesmith_and_postprocess_image",
         async buildStart() {
+          let { image, coord } = await new Promise((resolve, reject) => {
+            Spritesmith.run({ src: glob.sync(`./icon/${dir}/*.png`) }, function (err, result) {
+              if (err) { reject(err); }
+
+              let { width, height } = result.properties;
+              let coord_entries = Object.entries(result.coordinates).map(([img, coord]) =>
+                [path.basename(img, ".png"), { ...coord, total_width: width, total_height: height }]);
+              let coord = Object.fromEntries(coord_entries);
+
+              resolve({ image: result.image, coord: coord });
+            });
+          });
+
+          // write the sprite png and json coord
+          await fsp.writeFile(output_icon, image);
+          await fsp.writeFile(`./icon/${dir}.json`, JSON.stringify(coord));
+
+          // compress the png to png and webp, and report the size diff
           function filesize_mb(filename) {
             return (lstatSync(filename).size / 1024 / 1024).toFixed(2);
           }
-
           let size_before = filesize_mb(output_icon);
 
           const plugin_options = [
@@ -49,7 +61,7 @@ function get_sprite_plugins(mode) {
             size_after_png, "MB", "(png)",
             size_after_webp, "MB", "(webp)");
         },
-      }]
+      }
     } else {
       return [];
     }
